5282723d84de7c42d7384232898dd3a7c6693511
David Osvath <osvathdavid@yahoo.com>
2021-03-02 03:34:19 +0200

    finished homework

--- digitalClock.js
+++ digitalClock.js
@@ -12,27 +12,57 @@ const minutesParagraphs = document.querySelectorAll(".minutes p");
 let hours = 0;
 const hoursParagraphs = document.querySelectorAll(".hours p");
 
-setInterval(function () {
-  renderDigits(seconds, secondsParagraphs);
-  renderDigits(minutes, minutesParagraphs);
-  renderDigits(hours, hoursParagraphs);
+// document.getElementById("print-time").addEventListener("click", function(){
+//     console.log("The time is:", `${hours}:${minutes}:${seconds}`);
+// })
 
-  seconds++;
 
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
+document.getElementById("start-time").addEventListener("click", startTheTime);
+
+
+document.getElementById("stop-time").addEventListener("click", function(){
+  clearInterval(myTimer);
+  startCheck = 0;
+} );
+
+
+let myTimer;
+let startCheck = 0;
+
+function startTheTime(){
+  if (startCheck === 0) {
+    myTimer = setInterval(displayTime, 100);
+    startCheck++;
   }
+}
 
-  if (minutes === 60) {
-    minutes = 0;
-    hours++;
-  }
+document.getElementById("reset-time").addEventListener("click", resetTheTime);
 
-  if (hours === 24) {
-    hours = 0;
-  }
-}, 1000);
+document.getElementById("save-time").addEventListener("click", saveTheTime);
+
+
+
+              function displayTime() {
+                        renderDigits(seconds, secondsParagraphs);
+                        renderDigits(minutes, minutesParagraphs);
+                        renderDigits(hours, hoursParagraphs);
+
+                        seconds++;
+
+                        if (seconds === 60) {
+                          seconds = 0;
+                          minutes++;
+                        }
+
+                        if (minutes === 60) {
+                          minutes = 0;
+                          hours++;
+                        }
+
+                        if (hours === 24) {
+                          hours = 0;
+                        }
+                      }
 
 function renderDigits(nr, pList) {
   const stringDigits = nr + "";
@@ -45,4 +75,90 @@ function renderDigits(nr, pList) {
     pList[0].innerText = 0;
     pList[1].innerText = digitList[0];
   }
+}
+
+function resetTheTime() {
+  startCheck = 0;
+  clearInterval(myTimer);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  displayTime();
+
+}
+
+
+function saveTheTime(){
+  const newTime = document.createElement("div");
+  const newHoursHolder = document.createElement("div");
+  const newMinutesHolder = document.createElement("div");
+  const newSecondsHolder = document.createElement("div");
+  const newHoursLeftDigit = document.createElement("p");
+  const newHoursRightDigit = document.createElement("p");
+  const newMinutesLeftDigit = document.createElement("p");
+  const newMinutesRightDigit = document.createElement("p");
+  const newSecondsLeftDigit = document.createElement("p");
+  const newSecondsRightDigit = document.createElement("p");
+  const doubleDots = document.createElement("p");
+  const doubleDotsTwo = document.createElement("p");
+  doubleDots.innerText =":";
+  doubleDotsTwo.innerText = ":";
+
+  newHoursLeftDigit.innerText = 0;
+  newHoursRightDigit.innerText = 0;
+  newMinutesLeftDigit.innerText = 0;
+  newMinutesRightDigit.innerText = 0;
+  newSecondsLeftDigit.innerText = 0;
+  newSecondsRightDigit.innerText = 0;
+
+  newTime.classList.add("digital-clock");
+  newHoursHolder.classList.add("hours");
+  newMinutesHolder.classList.add("minutes");
+  newSecondsHolder.classList.add("seconds");
+
+  newTime.classList.add("digital-clock-save");
+  newHoursHolder.classList.add("hours-save");
+  newMinutesHolder.classList.add("minutes-save");
+  newSecondsHolder.classList.add("seconds-save");
+
+  newHoursHolder.appendChild(newHoursLeftDigit);
+  newHoursHolder.appendChild(newHoursRightDigit);
+
+
+  newMinutesHolder.appendChild(newMinutesLeftDigit);
+  newMinutesHolder.appendChild(newMinutesRightDigit);
+
+  newSecondsHolder.appendChild(newSecondsLeftDigit);
+  newSecondsHolder.appendChild(newSecondsRightDigit);
+
+
+  newTime.appendChild(newHoursHolder);
+  newTime.appendChild(doubleDots);
+  newTime.appendChild(newMinutesHolder);
+  newTime.appendChild(doubleDotsTwo);
+  newTime.appendChild(newSecondsHolder);
+
+
+  document.body.appendChild(newTime);
+
+  let savedHours = document.querySelectorAll(".hours-save p");
+  let savedMinutes = document.querySelectorAll(".minutes-save p");
+  let savedSeconds = document.querySelectorAll(".seconds-save p");
+
+                        renderDigits(seconds-1, savedSeconds);
+                        renderDigits(minutes, savedMinutes);
+                        renderDigits(hours, savedHours);
+
+                      
+  
+                        newTime.classList.remove("digital-clock-save");
+                        newHoursHolder.classList.remove("hours-save");
+                        newMinutesHolder.classList.remove("minutes-save");
+                        newSecondsHolder.classList.remove("seconds-save");
+
+
+
+  
+
+
 }
